406433fa424ed4655768435d89d610974b6da7e2
Leroy Russell <51133566+l-panther@users.noreply.github.com>
2022-09-06 00:18:41 +0100

    Add files via upload

--- books.php
+++ books.php
@@ -1,7 +1,6 @@
 <?php
 	include 'includes/head.html';
 	include 'includes/navigation.html';
-	include 'includes/banner.html';
 	include 'includes/books.html';
 	include 'includes/footer.php';
 ?>
--- delivery.php
+++ delivery.php
@@ -1,7 +1,6 @@
 <?php
 	include 'includes/head.html';
 	include 'includes/navigation.html';
-	include 'includes/banner.html';
 	include 'includes/delivery.html';
 	include 'includes/footer.php';
 ?>
