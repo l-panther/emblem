bc926ad21c9194bbb5439aa0aa05bef020659543
Leroy Russell <51133566+l-panther@users.noreply.github.com>
2022-10-09 18:41:15 +0100

    Add files via upload

--- scripts/hideNav.js
+++ scripts/hideNav.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
 	// Variables
 	let fullpath = window.location.pathname,
 		splitpath = fullpath.split("/"),
@@ -7,50 +7,51 @@ $(document).ready(function() {
 		path = removeLastElem.split(".")[0],
 		links = $("#primarynav li").children().addClass("w3-hover-none"),
 		dropdown = $("div .w3-dropdown-content").children().addClass("w3-hover-none");
-	
-	
+
+	scroll();
+
 	// Switch for active link
 	switch (path) {
 		case "":
 			setActive(".index");
-			break; 
+			break;
 		case "index":
 			setActive(".index");
-			break; 
+			break;
 		case "books":
 			setActive(".books");
-			break; 
+			break;
 		case "nineteen_century":
 			setActive(".nineteen");
-			break; 
+			break;
 		case "delivery":
 			setActive(".delivery");
-			break; 
+			break;
 		case "contact":
 			setActive(".contact");
-			break; 
+			break;
 	}
-	
-	
+
+
 	// Show navigation on small screen
-	$("#mini-nav").click(function() {
-		hideShow(); /* Get element and on click call function */	
+	$("#mini-nav").click(function () {
+		hideShow(); /* Get element and on click call function */
 	});
-	
+
 
 	// Show navigation on small scree
-	function setActive(id) {	
-		$("#primarynav a").removeClass("active"); 	
-		$(id).addClass("active"); 
-	} 
-	
+	function setActive(id) {
+		$("#primarynav a").removeClass("active");
+		$(id).addClass("active");
+	}
+
 
 	// Function to hide navigation on small dvices 
 	function hideShow() {
 		var navbar = $("#navbar").toggleClass("w3-hide"); /* Get element */
 	}
 
-	
+
 	// Scroll
 	function scroll() {
 
@@ -62,8 +63,8 @@ $(document).ready(function() {
 			if (window.pageYOffset <= 400) { // If window offset is less than or equal to number
 				skipLink.fadeOut(); //Fade out
 			} else { // Else
-				 skipLink.fadeIn(); // Fade in
-			} 
+				skipLink.fadeIn(); // Fade in
+			}
 		}
 		window.onscroll = getOffset; // On window scroll call getOffset
 	}
